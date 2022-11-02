ab3157652d077a1d1349f25fe3f750912d74ccd0
Stanford Tse <success7121995@gmail.com>
2022-11-03 00:52:39 +0800

    Search bar

--- js/function.js
+++ js/function.js
@@ -1,7 +1,15 @@
 // Menu
 const menu = document.getElementById('menu');
-const sideNav = document.getElementById('sidenav');
-
-menu.addEventListener('click', function() {
-    sideNav.classList.toggle('sidenav--expanded');
-})
+const sidenav = document.getElementById('sidenav');
+menu.addEventListener('click', function(e) {
+    sidenav.classList.toggle('sidenav--active');
+    e.preventDefault();   
+});
+// Search
+const search = document.getElementById('icon__search');
+const searchBar = document.getElementById('search-bar');
+const searchBox = document.getElementById('search-box');
+search.addEventListener('click', function() {
+    searchBar.classList.toggle('search-bar--active');
+    searchBox.classList.toggle('search-box--active');
+});
